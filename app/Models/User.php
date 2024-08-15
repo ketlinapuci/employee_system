b485e5b2e55a0a650648333e83f2b4b36b6b1e75
Ketlina Puci <ketlina.puci@evolutivo.io>
2024-08-15 21:44:33 +0200

    Create user roles

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,6 +20,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'role'
     ];
 
     /**
@@ -43,5 +44,25 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    /**
+     * Determine if the user is an admin.
+     *
+     * @return bool
+     */
+    public function isAdmin()
+    {
+        return $this->role === 'admin';
+    }
+
+    /**
+     * Determine if the user is an employee.
+     *
+     * @return bool
+     */
+    public function isEmployee()
+    {
+        return $this->role === 'employee';
     }
 }
